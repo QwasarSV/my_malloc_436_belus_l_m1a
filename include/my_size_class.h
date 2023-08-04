3eda1b0cd40e6d876bff6e19593e93c71f914e56
LBelus <lorris.belus@icloud.com>
2023-08-04 14:24:21 +0000

    add my size_class

---
+++ include/my_size_class.h
@@ -0,0 +1,34 @@
+#ifndef __MY_SIZE_CLASS_
+#define __MY_SIZE_CLASS_
+
+#include <main_header.h>
+
+#ifndef _SIZE_CLASS_DATA_
+#define _SIZE_CLASS_DATA_
+
+enum spacing {
+    SPACING_8 = 8,
+    SPACING_16 = 128,
+    SPACING_32 = 256,
+    SPACING_64 = 512,
+    SPACING_128 = 1024,
+    SPACING_256 = 2048,
+    SPACING_512 = 4096,
+    SPACING_1024 = 8192,
+    SPACING_2048 = 14336,
+    SPACING_COUNT = 10
+};
+
+extern int spacing_values[SPACING_COUNT];
+extern int size_class[SPACING_COUNT][9];
+#endif
+
+
+int binary_search(int array[], int left, int right, int value);
+int nearest_spacing(size_t size_request);
+int get_class_index(int value);
+int get_size_class(int class_index, size_t size);
+void test_class_retrieval_system(int size_req);
+
+
+#endif
---
+++ src/my_size_class/my_size_class.c
@@ -0,0 +1,122 @@
+#include <main_header.h>
+
+// https://stackoverflow.com/questions/426230/what-is-the-ld-preload-trick
+
+// enum spacing {
+//     SPACING_8 = 8,
+//     SPACING_16 = 128,
+//     SPACING_32 = 256,
+//     SPACING_64 = 512,
+//     SPACING_128 = 1024,
+//     SPACING_256 = 2048,
+//     SPACING_512 = 4096,
+//     SPACING_1024 = 8192,
+//     SPACING_2048 = 14336,
+//     SPACING_COUNT = 10
+// };
+
+int spacing_values[SPACING_COUNT] = {
+    SPACING_8,
+    SPACING_16,
+    SPACING_32,
+    SPACING_64,
+    SPACING_128,
+    SPACING_256,
+    SPACING_512,
+    SPACING_1024,
+    SPACING_2048
+};
+
+int size_class[SPACING_COUNT][9] = {
+    {8, 0},
+    {16, 32, 48, 64, 80, 96, 112, 128, 0},
+    {160, 192, 224, 256, 0},
+    {320, 384, 448, 512, 0},
+    {640, 768, 896, 1024, 0},
+    {1280, 1536, 1792, 2048, 0},
+    {2560, 3072, 3584, 4096, 0},
+    {5120, 6144, 7168, 8192, 0},
+    {10240, 12288, 14336, 0}
+};
+
+int binary_search(int array[], int left, int right, int value) 
+{
+    if (right >= left)
+    {
+        int mid = left + (right - left) / 2;
+        if (array[mid] == value)
+        {
+            return mid;
+        }
+  
+        if (array[mid] > value)
+        {
+            return binary_search(array, left, mid - 1, value);
+        }
+        else
+        {
+            return binary_search(array, mid + 1, right, value);
+        }
+    }
+    return -1;
+}
+
+int nearest_spacing(size_t size_request)
+{
+    int left = 0;
+    int right = SPACING_COUNT - 1;
+    int mid;
+
+    while (left <= right)
+    {
+        mid = left + (right - left) / 2;
+
+        if (spacing_values[mid] == size_request)
+        {
+            return spacing_values[mid];
+        }
+        if (spacing_values[mid] < size_request)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return spacing_values[left];
+}
+
+int get_class_index(int value)
+{
+    return binary_search(spacing_values, 0, SPACING_COUNT - 1, value);
+}
+
+int get_size_class(int class_index, size_t size)
+{
+    int index = 0;
+    while (size_class[class_index][index] != 0)
+    {
+        if (size_class[class_index][index] >= size)
+        {
+            return size_class[class_index][index];
+        }
+        index += 1;
+    }
+    return -1;
+}
+
+
+void test_class_retrieval_system(int size_req)
+{
+    printf("search for size_class match: %i \n", size_req);
+    int nearest_spacing_val = nearest_spacing(size_req);
+
+    printf("The nearest spacing, %i\n", nearest_spacing_val);
+    
+    int class_index = get_class_index(nearest_spacing_val);
+    printf("the class index, %i\n", class_index);
+    
+    int size_class_value = get_size_class(class_index, size_req);
+    printf("ouput_size_class, %i\n", size_class_value);
+}
